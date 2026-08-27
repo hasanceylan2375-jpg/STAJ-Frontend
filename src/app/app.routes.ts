fa2f57fd5f86cda7f0db2242fce839f53327e2ee
hasanceylan2375-jpg <hasanceylan2375@gmail.com>
2026-08-27 09:47:38 +0300

    Route Guard korumasını profil ve müşteri ekle sayfalarına uygula

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -8,50 +8,52 @@ export const routes: Routes = [
       import('./pages/home/home')
         .then(m => m.Home)
   },
-{
-  path: 'about',
-  loadComponent: () =>
-    import('./pages/about/about')
-      .then(m => m.About)
-},
-{
-  path: 'contact',
-  loadComponent: () =>
-    import('./pages/contact/contact')
-      .then(m => m.Contact)
-},
-{
-  path: 'features',
-  loadComponent: () =>
-    import('./pages/features/features')
-      .then(m => m.Features)
-},
-{
-  path: 'faq',
-  loadComponent: () =>
-    import('./pages/faq/faq')
-      .then(m => m.Faq)
-},
-{
-  path: 'login',
-  loadComponent: () =>
-    import('./pages/login/login')
-      .then(m => m.Login)
-},
-{
-  path: 'forbidden',
-  loadComponent: () =>
-    import('./pages/forbidden/forbidden')
-      .then(m => m.Forbidden)
-},
-{
-  path: 'profile',
-  loadComponent: () =>
-    import('./pages/profile/profile')
-      .then(m => m.Profile)
-},
+  {
+    path: 'about',
+    loadComponent: () =>
+      import('./pages/about/about')
+        .then(m => m.About)
+  },
+  {
+    path: 'contact',
+    loadComponent: () =>
+      import('./pages/contact/contact')
+        .then(m => m.Contact)
+  },
+  {
+    path: 'features',
+    loadComponent: () =>
+      import('./pages/features/features')
+        .then(m => m.Features)
+  },
+  {
+    path: 'faq',
+    loadComponent: () =>
+      import('./pages/faq/faq')
+        .then(m => m.Faq)
+  },
+  {
+    path: 'login',
+    loadComponent: () =>
+      import('./pages/login/login')
+        .then(m => m.Login)
+  },
+  {
+    path: 'forbidden',
+    loadComponent: () =>
+      import('./pages/forbidden/forbidden')
+        .then(m => m.Forbidden)
+  },
+  {
+    path: 'profile',
+    canActivate: [authGuard],
+    loadComponent: () =>
+      import('./pages/profile/profile')
+        .then(m => m.Profile)
+  },
   {
     path: 'musteri-ekle',
+    canActivate: [authGuard],
     loadComponent: () =>
       import('./pages/musteri-ekle/musteri-ekle')
         .then(m => m.MusteriEkle)
@@ -63,14 +65,14 @@ export const routes: Routes = [
       import('./pages/musteri-listele/musteri-listele')
         .then(m => m.MusteriListele)
   },
-   {
-  path: 'not-found',
-  loadComponent: () =>
-    import('./pages/not-found/not-found')
-      .then(m => m.NotFound)
-},
-{
-  path: '**',
-  redirectTo: 'not-found'
-}
+  {
+    path: 'not-found',
+    loadComponent: () =>
+      import('./pages/not-found/not-found')
+        .then(m => m.NotFound)
+  },
+  {
+    path: '**',
+    redirectTo: 'not-found'
+  }
 ];
