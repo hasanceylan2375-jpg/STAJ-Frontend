eaabefd0053d403ac653e93512bb24e01c55f515
hasanceylan2375-jpg <hasanceylan2375@gmail.com>
2026-08-27 15:20:21 +0300

    Add page parameters to customer list request

--- src/app/services/musteri.service.ts
+++ src/app/services/musteri.service.ts
@@ -1,33 +1,20 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpParams } from '@angular/common/http';
 
-@Injectable({
-  providedIn: 'root'
-})
+@Injectable({ providedIn: 'root' })
 export class MusteriService {
   private apiUrl = 'https://localhost:7233/api/Musteri';
-
   constructor(private http: HttpClient) {}
 
-  getMusteriler(search: string = '') {
-    let params = new HttpParams();
-
-    if (search.trim()) {
-      params = params.set('search', search.trim());
-    }
-
+  getMusteriler(search = '', page = 1, pageSize = 5) {
+    let params = new HttpParams()
+      .set('page', page)
+      .set('pageSize', pageSize);
+    if (search.trim()) params = params.set('search', search.trim());
     return this.http.get<any>(this.apiUrl, { params });
   }
 
-  musteriEkle(musteri: any) {
-    return this.http.post(this.apiUrl, musteri);
-  }
-
-  musteriGuncelle(id: number, musteri: any) {
-    return this.http.put(`${this.apiUrl}/${id}`, musteri);
-  }
-
-  musteriSil(id: number) {
-    return this.http.delete(`${this.apiUrl}/${id}`);
-  }
+  musteriEkle(musteri: any) { return this.http.post(this.apiUrl, musteri); }
+  musteriGuncelle(id: number, musteri: any) { return this.http.put(`${this.apiUrl}/${id}`, musteri); }
+  musteriSil(id: number) { return this.http.delete(`${this.apiUrl}/${id}`); }
 }
