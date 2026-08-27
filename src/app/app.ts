7260d1ecc37523e238850357fb30221ccc0b55d6
hasanceylan2375-jpg <hasanceylan2375@gmail.com>
2026-08-27 14:04:05 +0300

    Register toast component in app

--- src/app/app.ts
+++ src/app/app.ts
@@ -3,14 +3,14 @@ import { RouterOutlet } from '@angular/router';
 import { Sidebar } from './components/sidebar/sidebar';
 import { LoadingService } from './services/loading.service';
 import { Breadcrumb } from './components/breadcrumb/breadcrumb';
+import { Toast } from './components/toast/toast';
+
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Sidebar, Breadcrumb],
+  imports: [RouterOutlet, Sidebar, Breadcrumb, Toast],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
-
   constructor(public loadingService: LoadingService) {}
-
 }
