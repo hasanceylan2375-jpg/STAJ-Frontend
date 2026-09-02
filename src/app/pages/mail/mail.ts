87cf569a9685c377a8953b2562708be6a4b45c3d
hasanceylan2375-jpg <hasanceylan2375@gmail.com>
2026-09-02 10:58:56 +0300

    Add mail page component

---
+++ src/app/pages/mail/mail.ts
@@ -0,0 +1,44 @@
+import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { MailService } from '../../services/mail.service';
+
+@Component({
+  selector: 'app-mail',
+  standalone: true,
+  imports: [FormsModule],
+  templateUrl: './mail.html',
+  styleUrl: './mail.css'
+})
+export class Mail {
+  to = '';
+  subject = '';
+  body = '';
+  message = '';
+  sending = false;
+
+  constructor(private mailService: MailService) {}
+
+  send(): void {
+    if (!this.to || !this.subject || !this.body) {
+      this.message = 'Lütfen tüm alanları doldurun.';
+      return;
+    }
+
+    this.sending = true;
+    this.message = '';
+
+    this.mailService.sendMail({ to: this.to, subject: this.subject, body: this.body }).subscribe({
+      next: () => {
+        this.message = 'Mail başarıyla gönderildi.';
+        this.to = '';
+        this.subject = '';
+        this.body = '';
+        this.sending = false;
+      },
+      error: () => {
+        this.message = 'Mail gönderilirken bir hata oluştu.';
+        this.sending = false;
+      }
+    });
+  }
+}
