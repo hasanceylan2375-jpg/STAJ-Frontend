4bd563e6d3b2c2f668aa9b25eea458d381016d97
hasanceylan2375-jpg <hasanceylan2375@gmail.com>
2026-09-04 11:46:27 +0300

    Fix Konut imports

--- src/app/pages/konut-listele/konut-listele.ts
+++ src/app/pages/konut-listele/konut-listele.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, signal } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { KonutService } from '../../services/konut.service';
-@Component({selector:'app-konut-listele',imports:[FormsModule],templateUrl:'./konut-listele.html',styleUrl:'./konut-listele.css'})
+@Component({selector:'app-konut-listele',imports:[CommonModule,FormsModule],templateUrl:'./konut-listele.html',styleUrl:'./konut-listele.css'})
 export class KonutListele implements OnInit { konutlar=signal<any[]>([]);search='';constructor(private service:KonutService){} ngOnInit(){this.getir();} getir(){this.service.getir(this.search).subscribe(r=>this.konutlar.set(r));} }
