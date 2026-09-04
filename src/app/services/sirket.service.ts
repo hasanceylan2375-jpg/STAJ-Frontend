962dea04143024509599fa8b3cfc84447317b439
hasanceylan2375-jpg <hasanceylan2375@gmail.com>
2026-09-04 15:18:17 +0300

    Şirket ekleme servisini ekle

--- src/app/services/sirket.service.ts
+++ src/app/services/sirket.service.ts
@@ -5,6 +5,7 @@ export class SirketService {
   private apiUrl='https://localhost:7233/api/Sirket';
   constructor(private http:HttpClient){}
   getir(search=''){let params=new HttpParams();if(search.trim())params=params.set('search',search.trim());return this.http.get<any[]>(this.apiUrl,{params});}
+  ekle(sirket:any){return this.http.post<any>(this.apiUrl,sirket);}
   guncelle(id:number,sirket:any){return this.http.put<any>(`${this.apiUrl}/${id}`,sirket);}
   sil(id:number){return this.http.delete(`${this.apiUrl}/${id}`);}
 }
