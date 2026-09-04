7ca47eb892a58aac84f452ca0b8af49bd0450999
hasanceylan2375-jpg <hasanceylan2375@gmail.com>
2026-09-04 15:18:40 +0300

    Araç ekleme servisini ekle

--- src/app/services/arac.service.ts
+++ src/app/services/arac.service.ts
@@ -1,4 +1,4 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpParams } from '@angular/common/http';
 @Injectable({providedIn:'root'})
-export class AracService{private apiUrl='https://localhost:7233/api/Arac';constructor(private http:HttpClient){}getir(search=''){let params=new HttpParams();if(search.trim())params=params.set('search',search.trim());return this.http.get<any[]>(this.apiUrl,{params});}guncelle(id:number,arac:any){return this.http.put<any>(`${this.apiUrl}/${id}`,arac);}sil(id:number){return this.http.delete(`${this.apiUrl}/${id}`);}}
+export class AracService{private apiUrl='https://localhost:7233/api/Arac';constructor(private http:HttpClient){}getir(search=''){let params=new HttpParams();if(search.trim())params=params.set('search',search.trim());return this.http.get<any[]>(this.apiUrl,{params});}ekle(arac:any){return this.http.post<any>(this.apiUrl,arac);}guncelle(id:number,arac:any){return this.http.put<any>(`${this.apiUrl}/${id}`,arac);}sil(id:number){return this.http.delete(`${this.apiUrl}/${id}`);}}
