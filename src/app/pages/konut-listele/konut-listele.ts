8537e9d351e1881554f2538f4c1d03fdf6cf839d
hasanceylan2375-jpg <hasanceylan2375@gmail.com>
2026-09-04 11:45:58 +0300

    Add Konut list page

---
+++ src/app/pages/konut-listele/konut-listele.ts
@@ -0,0 +1,5 @@
+import { Component, OnInit, signal } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { KonutService } from '../../services/konut.service';
+@Component({selector:'app-konut-listele',imports:[FormsModule],templateUrl:'./konut-listele.html',styleUrl:'./konut-listele.css'})
+export class KonutListele implements OnInit { konutlar=signal<any[]>([]);search='';constructor(private service:KonutService){} ngOnInit(){this.getir();} getir(){this.service.getir(this.search).subscribe(r=>this.konutlar.set(r));} }
