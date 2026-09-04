5805183926aa04c243188858a0caea0e601a1ccf
hasanceylan2375-jpg <hasanceylan2375@gmail.com>
2026-09-04 15:18:50 +0300

    Şirket ekleme sayfası oluştur

---
+++ src/app/pages/sirket-ekle/sirket-ekle.ts
@@ -0,0 +1,6 @@
+import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { Router } from '@angular/router';
+import { SirketService } from '../../services/sirket.service';
+@Component({selector:'app-sirket-ekle',imports:[FormsModule],templateUrl:'./sirket-ekle.html',styleUrl:'./sirket-ekle.css'})
+export class SirketEkle{ad='';sektor='';email='';telefon='';logoUrl='';constructor(private service:SirketService,private router:Router){}kaydet(){this.service.ekle({ad:this.ad,sektor:this.sektor,email:this.email,telefon:this.telefon,logoUrl:this.logoUrl||null}).subscribe({next:()=>this.router.navigate(['/sirket-listele'])});}}
