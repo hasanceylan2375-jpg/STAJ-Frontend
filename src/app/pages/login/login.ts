b2cc0f4c84e935d2a7fb6cc80655fb0792276937
hasanceylan2375-jpg <hasanceylan2375@gmail.com>
2026-09-07 11:12:35 +0300

    feat(signalr): connect after successful login

--- src/app/pages/login/login.ts
+++ src/app/pages/login/login.ts
@@ -3,10 +3,12 @@ import { FormsModule } from '@angular/forms';
 import { HttpClient } from '@angular/common/http';
 import { Router, RouterLink } from '@angular/router';
 import { AuthService } from '../../services/auth/auth.service';
+import { SignalRService } from '../../services/signalr.service';
+
 @Component({ selector:'app-login', imports:[FormsModule, RouterLink], templateUrl:'./login.html', styleUrl:'./login.css' })
 export class Login {
   kullaniciAdi=''; sifre='';
   get isEnglish():boolean { return localStorage.getItem('language') === 'en-US'; }
-  private http=inject(HttpClient); private authService=inject(AuthService); private router=inject(Router);
-  login(){this.http.post<any>('https://localhost:7233/api/Auth/login',{kullaniciAdi:this.kullaniciAdi,sifre:this.sifre}).subscribe({next:(response)=>{this.authService.login(response.accessToken,response.refreshToken);this.router.navigate(['/musteri-listele']);},error:()=>{alert(this.isEnglish?'Invalid username or password.':'Kullanıcı adı veya şifre hatalı.');}});}
+  private http=inject(HttpClient); private authService=inject(AuthService); private router=inject(Router); private signalRService=inject(SignalRService);
+  login(){this.http.post<any>('https://localhost:7233/api/Auth/login',{kullaniciAdi:this.kullaniciAdi,sifre:this.sifre}).subscribe({next:async (response)=>{this.authService.login(response.accessToken,response.refreshToken);await this.signalRService.start();this.router.navigate(['/musteri-listele']);},error:()=>{alert(this.isEnglish?'Invalid username or password.':'Kullanıcı adı veya şifre hatalı.');}});}
 }
