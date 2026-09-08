3891afb4104e80ea7b592f6b3fccf46a139a9462
hasanceylan2375-jpg <hasanceylan2375@gmail.com>
2026-09-08 13:51:59 +0300

    feat: add lorem ipsum frontend demo page

---
+++ src/app/pages/frontend-lorem/frontend-lorem.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-frontend-lorem',
+  imports: [],
+  templateUrl: './frontend-lorem.html',
+  styleUrl: './frontend-lorem.css'
+})
+export class FrontendLorem {
+
+}
