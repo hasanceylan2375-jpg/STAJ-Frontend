08f661943240dc50c2c8e0e6fa529b24b79efc2c
hasanceylan2375-jpg <hasanceylan2375@gmail.com>
2026-09-10 11:53:39 +0300

    kredi hesaplama sonucunun aninda render edilmesini duzelt

--- src/app/pages/kredi-hesapla/kredi-hesapla.ts
+++ src/app/pages/kredi-hesapla/kredi-hesapla.ts
@@ -1,5 +1,5 @@
 import { CommonModule } from '@angular/common';
-import { Component, inject } from '@angular/core';
+import { ChangeDetectorRef, Component, inject } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpClient } from '@angular/common/http';
 
@@ -12,6 +12,7 @@ import { HttpClient } from '@angular/common/http';
 })
 export class KrediHesapla {
   private http = inject(HttpClient);
+  private cdr = inject(ChangeDetectorRef);
 
   krediTutari = 100000;
   vade = 120;
@@ -22,10 +23,15 @@ export class KrediHesapla {
   tip = 'EsitTaksitli';
   sonuc: any = null;
   hata = '';
+  hesapliyor = false;
 
   hesapla() {
+    if (this.hesapliyor) return;
+
     this.hata = '';
     this.sonuc = null;
+    this.hesapliyor = true;
+    this.cdr.detectChanges();
 
     this.http.post<any>('/api/Kredi/odeme-plani', {
       krediTutari: this.krediTutari,
@@ -36,8 +42,16 @@ export class KrediHesapla {
       periyot: this.periyot,
       tip: this.tip
     }).subscribe({
-      next: r => this.sonuc = r,
-      error: e => this.hata = e?.error?.mesaj || 'Ödeme planı oluşturulamadı.'
+      next: r => {
+        this.sonuc = r;
+        this.hesapliyor = false;
+        this.cdr.detectChanges();
+      },
+      error: e => {
+        this.hata = e?.error?.mesaj || 'Ödeme planı oluşturulamadı.';
+        this.hesapliyor = false;
+        this.cdr.detectChanges();
+      }
     });
   }
 
@@ -45,6 +59,7 @@ export class KrediHesapla {
     this.tip = tip;
     this.sonuc = null;
     this.hata = '';
+    this.cdr.detectChanges();
   }
 
   konutOrnegi() {
